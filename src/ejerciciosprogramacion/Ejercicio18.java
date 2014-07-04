728f46e14c491d726bca692671fbf866b70fb464
luisaholguin <luisa-ro1992@hotmail.com>
2014-07-04 16:36:54 -0500

    Ejercicio18

---
+++ src/ejerciciosprogramacion/Ejercicio18.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejerciciosprogramacion;
+
+/**
+ *
+ * @authora: Luisa Holguin
+ */
+public class Ejercicio18 {
+        public String[] Ordenar(String[] valores){
+        for(int i=0;i<valores.length;i++) {
+            for(int j=0;j<valores.length-i;j++) {
+                if (valores[j].compareTo(valores[j+1])>0) {
+                    String aux;
+                    aux=valores[j];
+                    valores[j]=valores[j+1];
+                    valores[j+1]=aux;
+                }
+            }
+        }
+        return valores;
+    }
+    
+}
+
